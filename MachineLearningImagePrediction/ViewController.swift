d5f49b28ba1fd5605841a977d0de9ca3c944fda4
aKadirCevik <abdulkadircevik60@gmail.com>
2022-11-03 19:45:13 +0300

    Initial Commit

---
+++ MachineLearningImagePrediction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningImagePrediction
+//
+//  Created by Abdulkadir Cevik on 3.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
